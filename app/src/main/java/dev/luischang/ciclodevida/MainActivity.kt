c9ce1a6fe26be38f44b496102afecbea642a3a04
Luis Chang <luis.chang@outlook>
2024-08-31 11:57:51 -0500

    Ciclo de vida del MainActivity

--- app/src/main/java/dev/luischang/ciclodevida/MainActivity.kt
+++ app/src/main/java/dev/luischang/ciclodevida/MainActivity.kt
@@ -1,12 +1,15 @@
 package dev.luischang.ciclodevida
 
 import android.os.Bundle
+import android.util.Log
+
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+    private val tag: String = "INF-STATE"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -17,5 +20,26 @@ class MainActivity : AppCompatActivity() {
             insets
         }
         //Hola mundo - Luis
+        Log.i(tag,"MainActivity-->onCreate")
+    }
+    override fun onStart() {
+        super.onStart()
+        Log.i(tag,"MainActivity-->onStart")
+    }
+    override fun onResume() {
+        super.onResume()
+        Log.i(tag,"MainActivity-->onResume")
+    }
+    override fun onPause() {
+        super.onPause()
+        Log.i(tag,"MainActivity-->onPause")
+    }
+    override fun onStop() {
+        super.onStop()
+        Log.i(tag,"MainActivity-->onStop")
+    }
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.i(tag,"MainActivity-->onDestroy")
     }
 }
